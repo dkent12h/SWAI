fe2e916b4a9a7f9d79ac6c6c98ea5475cd4c6754
dkent12h <52147310+dkent12h@users.noreply.github.com>
2023-01-13 07:12:36 +0900

    Create python_translate(eng).py

---
+++ python code/speech recognition/python_translate(eng).py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+
+from os import path
+
+from pprint import pprint
+
+audio_file = path.join(path.dirname(path.realpath(__file__)), "knue.wav")
+
+r = sr.Recognizer()
+
+with sr.AudioFile(audio_file) as source :
+    audio = r.record(source)
+
+try:
+    text = r.recognize_google(audio, show_all=True)
+    pprint (text)
+
+except:
+    print ("Didn't work.")
